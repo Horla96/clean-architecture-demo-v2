6393bd81966f9ecd4073754a9bdc23059f51bd1d
Horla96 <sexysulai@gmail.com>
2025-11-25 15:27:13 +0100

    Add BlogController GET Action, ApiControllerBase, and Initial EF Core Migration

---
+++ clean-arch-demo-v2.Api/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using clean_arch_demo_v2.Application.Blogs.Queries.GetBlogs;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace clean_arch_demo_v2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ApiControllerBase
+    {
+       
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var blogs = await Mediator.Send(new GetBlogQuery());
+            return Ok(blogs);
+        }
+    }
+}
